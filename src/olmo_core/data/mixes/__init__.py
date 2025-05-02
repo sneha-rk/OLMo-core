c13fdb60476a6e0202300cef7ba292d1791b4906
sneha-rk <snehareddyk96@gmail.com>
2025-05-02 15:43:39 -0700

    Adds example files compatible with cluster and test data

--- src/olmo_core/data/mixes/__init__.py
+++ src/olmo_core/data/mixes/__init__.py
@@ -37,6 +37,7 @@ class DataMix(DataMixBase):
 
     # Pretraining mixes
     OLMoE_mix_0824 = "OLMoE-mix-0824"
+    OLMoE_test = "OLMoE-test"
     dolma17 = "dolma17"
     OLMo_mix_0625 = "OLMo-mix-0625"
     OLMo_mix_0625_150Bsample = "OLMo-mix-0625-150Bsample"
